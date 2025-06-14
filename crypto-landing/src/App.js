6c30ea651c3061dcc24d6dbc387db810ae1bc994
Dam17an <tomcdamijan@gmail.com>
2025-06-14 19:56:12 +0200

    Done Hero

---
+++ crypto-landing/src/App.js
@@ -0,0 +1,15 @@
+import Hero from './Components/Hero/Hero.jsx';
+import FeatureCards from "./Components/FeatureCards/FeatureCards";
+import IntroSection from "./Components/IntroSection/IntroSection";
+
+function App() {
+    return (
+        <div className="App">
+            <Hero />
+            <FeatureCards />
+            <IntroSection />
+        </div>
+    );
+}
+
+export default App;
---
+++ crypto-landing/src/Components/Hero/Hero.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Hero.scss';
+
+const Hero = () => {
+    return (
+        <div className="hero">
+            {/* Header with centered nav and right-aligned buttons */}
+            <div className="hero__header">
+                <div className="hero__header-content">
+                    <ul className="hero__nav">
+                        <li>Buy / Sell</li>
+                        <li>Grow</li>
+                        <li>Markets</li>
+                        <li>Business</li>
+                        <li>Support</li>
+                    </ul>
+
+                    <div className="hero__buttons">
+                        <button className="btn btn--white">Sign in</button>
+                        <button className="btn btn--gradient">Sign up</button>
+                    </div>
+                </div>
+            </div>
+
+            {/* Centered hero text and CTA */}
+            <div className="hero__content">
+                <h1>We make crypto<br />clear and simple</h1>
+                <button className="btn btn--gradient btn--large">Get Started</button>
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
